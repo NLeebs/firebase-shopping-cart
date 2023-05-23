be42b1401c3ff946f436b622170ea894e062679b
NLeebs <lieberman.nathan@gmail.com>
2023-05-23 16:55:47 -0400

    Goes through all DB stores and adds to shopping UL

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ const appSettings = {
   databaseURL: "https://shopping-list-4dae2-default-rtdb.firebaseio.com/",
 };
 
-// Get elements
+/////////// Get elements ///////////
 const btnAddCartEl = document.getElementById("btn-addCart");
 const inputAddCartEl = document.getElementById("input-addCart");
 const selectStoreEl = document.getElementById("select-store");
+const shoppingListEl = document.getElementById("shopping-list");
 const aldiShoppingListEl = document.getElementById("aldi-shopping-list");
+const mMarketShoppingListEl = document.getElementById(
+  "mediterranean-market-shopping-list"
+);
 
-//Init App
+/////////// Init App ///////////
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 
@@ -29,14 +33,16 @@ storeArr.forEach((store) => {
   selectStoreEl.append(newEl);
 });
 
-// Create DB references
+/////////// Create DB references ///////////
 const DBRefObject = {};
 storeArr.forEach((store) => {
   const varName = `itemsIn${store.replace(" ", "")}DB`;
   DBRefObject[varName] = ref(database, `items/${store.replace(" ", "-")}`);
 });
+//DBRefObject.storesInDB = ref(database, "items");
+console.log(DBRefObject);
 
-// General Functions
+/////////// General Functions ///////////
 const resetInputValue = (input) => {
   input.value = "";
 };
@@ -45,6 +51,7 @@ const appendListItem = (list, listItem) => {
   const [listItemID, listItemValue] = listItem;
   const newEl = document.createElement("li");
   newEl.textContent = `${listItemValue}`;
+  // Delete functionality
   newEl.addEventListener("click", () => {
     const exactLocationOfItemInDB = ref(database, `items/${listItemID}`);
     remove(exactLocationOfItemInDB);
@@ -52,34 +59,50 @@ const appendListItem = (list, listItem) => {
   list.append(newEl);
 };
 
-const clearShoppingLists = (list) => {
-  list.innerHTML = "";
+const clearShoppingLists = () => {
+  shoppingListEl.innerHTML = "";
 };
 
-// Get values from DB
-onValue(DBRefObject.itemsInAldiDB, function (snapshot) {
-  if (snapshot.exists()) {
-    clearShoppingLists(aldiShoppingListEl);
-    const databaseItemsArray = Object.entries(snapshot.val());
+function storeValueToPath(str) {
+  const words = str.split("-"); // Split the string into an array of words
+  const capitalizedWords = words.map((word) => {
+    const firstLetter = word.charAt(0).toUpperCase(); // Get the first letter of each word and capitalize it
+    const restOfWord = word.slice(1); // Get the remaining characters of each word
+    return `${firstLetter}${restOfWord}`; // Concatenate the capitalized first letter with the rest of the word
+  });
+  return capitalizedWords.join(""); // Join the array of capitalized words back into a string
+}
 
-    for (let i = 0; i < databaseItemsArray.length; i++) {
-      const currentItem = databaseItemsArray[i];
-      appendListItem(aldiShoppingListEl, currentItem);
+/////////// Get values from DB ///////////
+Object.keys(DBRefObject).forEach((DBref) => {
+  onValue(DBRefObject[DBref], function (snapshot) {
+    if (snapshot.exists()) {
+      // clearShoppingLists();
+      const databaseItemsArray = Object.entries(snapshot.val());
+      console.log(databaseItemsArray);
+
+      for (let i = 0; i < databaseItemsArray.length; i++) {
+        const currentItem = databaseItemsArray[i];
+        console.log(currentItem);
+        appendListItem(shoppingListEl, currentItem);
+      }
+    } else {
+      shoppingListEl.innerHTML = `<p>No items added...</p>`;
     }
-  } else {
-    ulShoppingList.innerHTML = `<p>No items added...</p>`;
-  }
+  });
 });
 
 // Event handlers
 const addCartHandler = () => {
-  let inputValue = inputAddCartEl.value;
+  const inputValue = inputAddCartEl.value;
   resetInputValue(inputAddCartEl);
 
   if (!inputValue) return;
 
+  const storeValue = storeValueToPath(selectStoreEl.value);
+
   // push to database
-  push(DBRefObject.itemsInAldiDB, inputValue);
+  push(DBRefObject[`itemsIn${storeValue}DB`], inputValue);
 };
 
 // Add Event Listners
