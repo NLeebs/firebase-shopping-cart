5c1ec8541b2308068ba9f25ef5372398d958be5a
NLeebs <lieberman.nathan@gmail.com>
2023-06-07 14:01:47 -0400

    Added hummus and abodo recipes

--- store/recipes.js
+++ store/recipes.js
@@ -1,29 +1,35 @@
 const recipeObj = {
-  "Standard Veggies": {
+  "Abodo Chicken": {
     Aldi: [
-      { item: "Celery", type: "Produce", amount: 1 },
-      { item: "Carrots", type: "Produce", amount: 1 },
-      { item: "Cabbage", type: "Produce", amount: 1 },
+      { item: "Chicken Thighs", type: "refridge", amount: 1 },
+      { item: "Garlic", type: "produce", amount: 1 },
+      { item: "Jalapeno", type: "produce", amount: 1 },
+    ],
+    "Mediterranean Market": [
+      { item: "Bay Leaves", type: "dry-goods", amount: 1 },
+    ],
+    Saraga: [
+      { item: "Soy Sauce", type: "dry-goods", amount: 1 },
+      { item: "Vinegar", type: "dry-goods", amount: 1 },
     ],
   },
-  "Example Recipe": {
-    Aldi: [{ item: "Test", type: "Produce", amount: 1 }],
-    "Mediterranean Market": [
-      { item: "Test", type: "Produce", amount: 1 },
-      { item: "Test 2", type: "Refridge", amount: 1 },
+  Hummus: {
+    Aldi: [
+      { item: "Olive Oil", type: "dry-goods", amount: 1 },
+      { item: "Lemon Juice", type: "dry-goods", amount: 1 },
+      { item: "Garlic", type: "produce", amount: 1 },
+      { item: "Chickpeas", type: "dry-goods", amount: 2 },
     ],
-    Saraga: [{ item: "Test", type: "Produce", amount: 1 }],
+    "Mediterranean Market": [{ item: "Tahini", type: "dry-goods", amount: 1 }],
+  },
+  "Standard Veggies": {
+    Aldi: [
+      { item: "Celery", type: "produce", amount: 1 },
+      { item: "Onion", type: "produce", amount: 1 },
+      { item: "Carrots", type: "produce", amount: 1 },
+      { item: "Cabbage", type: "produce", amount: 1 },
+    ],
   },
 };
 
 export default recipeObj;
-
-/*
-
-"item": {".validate" : "true"},
-        "type": {".validate" : "true"},
-        "amount": {".validate" : "true"},
-        "$other": {".validate" : "false"},
-
-
-*/
